fadcea65333c7aa830e7a325002cbff0befc5e98
Alfonso <adomenechreal@gmail.com>
2021-01-11 19:04:46 +0100

    Add common files and config

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
+import router from './routes';
 
 // mongoose.Promise = global.Promise;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(bodyParser.text());
+app.use('/', router);
 mongoose
   .connect(`mongodb://localhost:27017/robots`, {
     useNewUrlParser: true,
