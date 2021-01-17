586aa03977a68989bb839ac9da50602400a10d6d
Alfonso <adomenechreal@gmail.com>
2021-01-17 01:10:47 +0100

    Update input module

--- src/inputs/services/input.service.ts
+++ src/inputs/services/input.service.ts
@@ -2,16 +2,17 @@ import { Orientation } from '../../robots/enums';
 import { IInput } from '../interfaces';
 import { Input } from '../models';
 
-export const createInstructions = async (data: string): Promise<any> => {
+export const createInstructions = async (data: string): Promise<IInput[]> => {
   try {
     const [xDim, yDim] = data
       .split('\n')[0]
       .split(' ')
       .map((el) => parseInt(el));
 
-    let inputPlain = {};
+    const inputs = [];
 
     const inputsRaw = data.split('\n');
+
     for (let i = 1; i < inputsRaw.length; i += 2) {
       const [xCoor, yCoord, orientation] = inputsRaw[i].split(' ');
       // TODO añadir constantes y tipo de error
@@ -19,7 +20,7 @@ export const createInstructions = async (data: string): Promise<any> => {
         throw new Error('Too big');
       }
 
-      inputPlain = {
+      const input: IInput = new Input({
         dimensions: { xCoord: xDim, yCoord: yDim },
         position: {
           xCoord: parseInt(xCoor),
@@ -27,23 +28,21 @@ export const createInstructions = async (data: string): Promise<any> => {
           orientation: (orientation as any) as Orientation
         },
         instructions: inputsRaw[i + 1].split('')
-      };
-      const input: IInput = new Input(inputPlain);
+      });
       await input.save();
+      inputs.push(input);
     }
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
 
-  // return Input.find().exec();
+    return inputs;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-export const getInputs = (): Promise<IInput[]> => {
+export const findInputs = (): Promise<IInput[]> => {
   try {
     return Input.find().exec();
   } catch (error) {
-    return error;
+    throw new Error('error');
   }
 };
-// const parseOrientation = (orientation: string)
